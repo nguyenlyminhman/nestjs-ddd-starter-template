f3d2d7ce56174651d36df7ee81025c8527ab8e36
man nguyen <minhman2.nguyen@mafc.com.vn>
2026-01-15 14:06:21 +0700

    add all

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { SharedModule } from './modules/shared/shared.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServerConfigService } from './modules/shared/server-config.service';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,6 +15,21 @@ import { ServerConfigService } from './modules/shared/server-config.service';
       // load: [() => ({
       //   defaultLang: 'vi',
       // })],
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ServerConfigService) => ({
+        fallbackLanguage: configService.fallbackLanguage,
+        loaderOptions: {
+          path: configService.isDevelopment ? join(process.cwd(), 'src/i18n') : join(__dirname, '/i18n'),
+          watch: configService.isDevelopment,
+        },
+      }),
+      imports: [SharedModule],
+      inject: [ServerConfigService],
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
     }),
     SharedModule],
   controllers: [AppController],
